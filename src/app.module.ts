4f7cdf5c660ca50e6b0d156014f777582d47b3eb
Alexandr Chubukov <achubukov@htc-cs.ru>
2024-01-22 10:21:28 +0400

    Add files module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service'
 import { AppController } from './app.controller'
 
 import { AuthModule } from './auth/auth.module'
+import { FilesModule } from './files/files.module'
 import { UsersModule } from './users/users.module'
 import { JwtStrategy } from './auth/strategies/jwt.strategy'
 import { ReviewModule } from './review/review.module'
@@ -21,6 +22,7 @@ import { TopPageModule } from './top-page/top-page.module'
     ReviewModule,
     ProductModule,
     TopPageModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService, JwtStrategy],
